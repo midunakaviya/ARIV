669b4ac98ef6e9a969cee6d2a96a04953679fcf9
Midunakaviya Mohanraju <midunakaviya.mohanraju@s2024.tu-chemnitz.de>
2026-03-24 14:04:27 +0100

    Updated main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+console.log("ENV:", import.meta.env);
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
